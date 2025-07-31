2c5ffbffb424efaab4095e44db38538fe4dca44e
Logan-Delaney <logan_delaney@yahoo.com>
2025-07-30 23:34:12 -0400

    mobile nav items fix

--- src/Sections/Navbar.jsx
+++ src/Sections/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
                     <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors z-100">
                         WLD
                     </a>
-                    <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle Menu">
+                    <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex z-100" aria-label="Toggle Menu">
                         <img src={isOpen ? "assets/x-solid.svg" : "assets/bars-solid.svg"} alt="toggle" className="w-6 h-6" />
                     </button>
 
@@ -36,11 +36,21 @@ const Navbar = () => {
                     </nav>
                 </div>
             </div>
-            <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
-                <nav className="p-5">
-                    <NavItems/>
-                </nav>
-            </div>
+            {isOpen && (
+                <div className="fixed top-0 left-0 w-full h-screen bg-[#121212] z-50 flex flex-col items-center justify-center">
+                    <nav>
+                        <ul className="flex flex-col gap-8 items-center text-2xl">
+                            {navLinks.map(({id, href, name}) => (
+                                <li key={id}>
+                                    <a href={href} className="text-neutral-200 hover:text-blue-400 transition-colors">
+                                        {name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+            )}
         </header>
     )
 }
